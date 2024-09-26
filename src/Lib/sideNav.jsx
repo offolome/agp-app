157c442aacf7e988d8f81ad567b76639bb116783
unknown <offado>
2024-09-26 17:43:26 +0000

    Gestion

---
+++ src/Lib/sideNav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SideNav = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <a className="nav-link">
+                        <img src="" alt="" />
+                        <h1><strong>AGP-TECH</strong></h1>
+                    </a>
+                </div>
+            </nav>
+                
+            <div>
+                <ul className="active">
+                    <li><a href="#">Tableau de bord</a></li>
+                    <li><a href="#">Client</a></li>
+                    <li><a href="#">Vêtements</a></li>
+                    <li><a href="#">Rendez-vous</a></li>
+                    <li><a href="#">Message</a></li>
+                    <li><a href="#">Utilisateurs</a></li>
+                    <li><a href="#">Paramètres</a></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default SideNav;
